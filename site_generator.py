ab22f84b0f24b83157a77914f7aef1a6e739c2b3
Vladimir Grishkin <vpgrishkin@gmail.com>
2017-06-25 00:37:01 +0300

    fixed: cleaned funcs, changed load_md/config

--- site_generator.py
+++ site_generator.py
@@ -14,17 +14,12 @@ CONFIG = 'config.json'
 ENCODING = 'utf-8'
 
 
-def load_config(file_path):
-    with open(file_path, encoding=ENCODING) as config_file:
-        return json.loads(config_file.read())
+def load_file(file_path, default_encoding):
+    with open(file_path, encoding=default_encoding) as file:
+        return file.read()
 
 
-def load_md(file_path):
-    with open(file_path, encoding=ENCODING) as md_file:
-        return md_file.read()
-
-
-def get_jinja_template(filename, path=TEMPLATES_PATH):
+def get_jinja_template(filename, path):
     env = Environment(loader=FileSystemLoader(path),
                       auto_reload=True,
                       trim_blocks=True,
@@ -39,35 +34,39 @@ def add_source_html(articles):
     return articles_source_html
 
 
-def write_html_articles(articles, article_template):
+def write_html_articles(articles, article_template, article_path, default_encoding, site_path):
     for article in articles:
-        md_article_path = os.path.join(ARTICLES_PATH, article['source'])
-        content = markdown.markdown(load_md(md_article_path))
+        md_article_path = os.path.join(article_path, article['source'])
+        content = markdown.markdown(load_file(md_article_path, default_encoding))
         article_html = article_template.render(content=content,
                                                title=article['title'])
-        html_article_path = os.path.join(OUTPUT_SITE_PATH, article['source_html'])
+        html_article_path = os.path.join(site_path, article['source_html'])
         html_article_dir = os.path.dirname(html_article_path)
 
         if not os.path.exists(html_article_dir):
             os.makedirs(html_article_dir)
 
-        write_html_file(html_article_path, article_html)
+        write_html_file(html_article_path, article_html, default_encoding)
 
 
-def write_html_file(path, article_html):
-    with open(path, 'w', encoding=ENCODING) as html_file:
+def write_html_file(path, article_html, default_encoding):
+    with open(path, 'w', encoding=default_encoding) as html_file:
         html_file.write(article_html)
 
 
 if __name__ == '__main__':
-    config = load_config(CONFIG)
+    config = json.loads(load_file(CONFIG, ENCODING))
     articles = config['articles']
     topics = config['topics']
-    article_template = get_jinja_template('article.html')
+    article_template = get_jinja_template('article.html', TEMPLATES_PATH)
     articles_source_html = add_source_html(articles)
-    write_html_articles(articles_source_html, article_template)
-    index_template = get_jinja_template('index.html')
+    write_html_articles(articles_source_html,
+                        article_template,
+                        ARTICLES_PATH,
+                        ENCODING,
+                        OUTPUT_SITE_PATH)
+    index_template = get_jinja_template('index.html', TEMPLATES_PATH)
     index_html_content = index_template.render(topics=topics,
                                                articles=articles_source_html)
     index_html_path = os.path.join(OUTPUT_SITE_PATH, 'index.html')
-    write_html_file(index_html_path, index_html_content)
+    write_html_file(index_html_path, index_html_content, ENCODING)
